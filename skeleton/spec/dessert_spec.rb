8cc50e01ed1b2bd50c8befb76dbf8aaeebeedd16
thetrevlore <trevorstorey23@gmail.com>
2017-05-09 21:20:39 -0400

    Make Rspec tests

---
+++ skeleton/spec/dessert_spec.rb
@@ -0,0 +1,78 @@
+require 'rspec'
+require 'dessert.rb'
+
+=begin
+Instructions: implement all of the pending specs (the `it` statements
+without blocks)! Be sure to look over the solutions when you're done.
+=end
+
+describe Dessert do
+  let(:chef) { double("chef", name: "Daniel") }
+  let(:ice_cream) { Dessert.new("ice_cream", 3, chef)}
+
+  describe "#initialize" do
+    it "sets a type" do
+      expect(ice_cream.type).to eq("ice_cream")
+    end
+
+    it "sets a quantity" do
+      expect(ice_cream.quantity).to eq(3)
+    end
+
+    it "starts ingredients as an empty array" do
+      expect(ice_cream.ingredients).to eq([])
+    end
+
+    it "raises an argument error when given a non-integer quantity" do
+      expect { Dessert.new('brownie', 'tons', chef) }.to raise_error
+    end
+  end
+
+  describe "#add_ingredient" do
+    it "adds an ingredient to the ingredients array" do
+      ice_cream.add_ingredient("sugar")
+      expect(ice_cream.ingredients).to include("sugar")
+    end
+  end
+
+  describe "#mix!" do
+    it "shuffles the ingredient array" do
+      ingredients = ["chocolate", "flour", "egg", "sugar", "butter"]
+
+      ingredients.each do |ingredient|
+        ice_cream.add_ingredient(ingredient)
+      end
+
+      expect(ice_cream.ingredients).to eq(ingredients)
+      ice_cream.mix!
+      expect(ice_cream.ingredients).not_to eq(ingredients)
+      expect(ice_cream.ingredients.sort).to eq(ingredients.sort)
+    end
+  end
+
+  describe "#eat" do
+    it "subtracts an amount from the quantity" do
+      ice_cream.eat(1)
+      expect(ice_cream.quantity).to eq(2)
+    end
+
+
+    it "raises an error if the amount is greater than the quantity" do
+      expect{ice_cream.eat(7)}.to raise_error("not enough left!")
+    end
+  end
+
+  describe "#serve" do
+    it "contains the titleized version of the chef's name" do
+      allow(chef).to receive(:titleize).and_return("Chef Daniel the Great Baker")
+      expect(ice_cream.serve).to eq("Chef Daniel the Great Baker has made 3 ice_creams!")
+    end
+  end
+
+  describe "#make_more" do
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      expect(chef).to receive(:bake).with(ice_cream)
+      ice_cream.make_more
+    end
+  end
+end
